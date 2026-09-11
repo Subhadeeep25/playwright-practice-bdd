60fbfc2a28524ea42f7e76d37f14f8168a07147d
Subhadeeep25 <subhadeep25aeccse@gmail.com>
2026-09-11 23:56:08 +0530

    Fix: removed extra features

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,12 +13,12 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
-    ['html', { open: 'never' }],
-    ['list']
+    ['html'],
+    ['github']
   ],
   use: {
     baseURL: 'https://practice.expandtesting.com',
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     headless: true,
